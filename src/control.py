01836f8722c068be781828215be7e5adfbfc6016
ericzn248 <ericzn248@gmail.com>
2025-10-17 21:10:33 -0400

    some fixes

--- src/control.py
+++ src/control.py
@@ -10,6 +10,7 @@ kd = 7.0 #TODO
 ki = 0.0 #TODO
 servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
 prev_error = 0.0
+prev_angle = 0.0
 
 
 # This code can input desired velocity from the user.
@@ -24,8 +25,7 @@ vel_input = 15.0	#TODO
 # Publisher for moving the car.
 # TODO: Use the coorect topic /car_x/offboard/command. The multiplexer listens to this topic
 command_pub = rospy.Publisher('/car_5/offboard/command', AckermannDrive, queue_size = 1)
-def getPrevInfo(msg):
-	prev_angle = msg.steering.angle
+
 def control(data):
 	global prev_error
 	global vel_input
@@ -39,17 +39,18 @@ def control(data):
 
 	## Your PID code goes here
 	#TODO: Use kp, ki & kd to implement a PID controller
-	rospy.Subscriber('/car_5/offboard/command', AckermannDrive, getPrevInfo)
 	# 1. Scale the error
-	error = error * 1
+
 	# 2. Apply the PID equation on error to compute steering
-	v_theta = kp * data + kd(prev_error - data)
+	v_theta = kp * data.pid_error + kd*(prev_error - data.pid_error)
 	angle = prev_angle - v_theta
 
-	command.steering_angle_velocity= v_theta
+	
 
 	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
+
 	command = AckermannDrive()
+	command.steering_angle_velocity= v_theta
 
 	# TODO: Make sure the steering value is within bounds [-100,100]
 	if (angle > -100) and (angle < 100):
@@ -73,6 +74,7 @@ def control(data):
 	rospy.loginfo(command.acceleration)
 	command.acceleration = 0
 
+	prev_angle = command.steering_angle
 	command_pub.publish(command)
 
 if __name__ == '__main__':
@@ -89,4 +91,5 @@ if __name__ == '__main__':
 	rospy.init_node('pid_controller', anonymous=True)
     # subscribe to the error topic
 	rospy.Subscriber("error", pid_input, control)
+
 	rospy.spin()
