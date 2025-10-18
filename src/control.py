44bc21e1c6cc1a3870dcd07fce283919296059c5
sheri-na <sherina.l.williams@gmail.com>
2025-10-18 17:16:46 -0400

    testing kp

--- src/control.py
+++ src/control.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import math
 import rospy
+import numpy as np
 from race.msg import pid_input
 from ackermann_msgs.msg import AckermannDrive
 
 # PID Control Params
-kp = 0.0 #TODO
+# begin with kp tuning have ki and kd to 0
+kp = 10.0 #TODO
+#once the system starts to oscillate around the set point add kd
 kd = 0.0 #TODO
+# is a steady state error remains gradually increase Ki
 ki = 0.0 #TODO
 servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
 prev_error = 0.0
@@ -19,11 +23,11 @@ prev_error = 0.0
 # 25: Slow and steady
 # 35: Nice Autonomous Pace
 # > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
-vel_input = 0.0	#TODO
+vel_input = 15.0	#TODO
 
 # Publisher for moving the car.
-# TODO: Use the coorect topic /car_x/offboard/command. The multiplexer listens to this topic
-command_pub = rospy.Publisher('/car_9/offboard/command', AckermannDrive, queue_size = 1)
+# TODO: Use the correct topic /car_x/offboard/command. The multiplexer listens to this topic
+command_pub = rospy.Publisher('/car_5/offboard/command', AckermannDrive, queue_size = 1)
 
 def control(data):
 	global prev_error
@@ -36,18 +40,18 @@ def control(data):
 
 	## Your PID code goes here
 	#TODO: Use kp, ki & kd to implement a PID controller
-
+    #Define setpoint
 	# 1. Scale the error
+    error = data.pid_error
 	# 2. Apply the PID equation on error to compute steering
-
+    steering_output = kp * error
 	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
 	command = AckermannDrive()
-
 	# TODO: Make sure the steering value is within bounds [-100,100]
-	command.steering_angle = angle
+	command.steering_angle = np.clip(angle, -100, 100)
 
 	# TODO: Make sure the velocity is within bounds [0,100]
-	command.speed = vel_input
+	command.speed = np.clip(vel_input, 0, 100)
 
 	# Move the car autonomously
 	command_pub.publish(command)
