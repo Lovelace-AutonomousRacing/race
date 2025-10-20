e3722a0ad0005bc9b87236ba7a07bde8e7f153b3
sheri-na <sherina.l.williams@gmail.com>
2025-10-20 01:20:08 -0400

    test

--- src/control.py
+++ src/control.py
@@ -9,19 +9,12 @@ kp = 0.0 #TODO
 kd = 0.0 #TODO
 ki = 0.0 #TODO
 servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
-prev_error = 0.0
+prev_error = 0.0     # previous PID error
+prev_angle = 0.0     # accumulated steering angle
 
-
-
-
-# This code can input desired velocity from the user.
-# velocity must be between [0,100] to move forward.
-# The following velocity values correspond to different speed profiles.
-# 15: Very Slow (Good for debug mode)
-# 25: Slow and steady
-# 35: Nice Autonomous Pace
-# > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
-vel_input = 15.0	#TODO
+# Velocity parameters
+vel_input = 15.0	# desired base velocity
+vel_scale = 0.0      # scale factor for reducing speed with error
 
 # Publisher for moving the car.
 command_pub = rospy.Publisher('/car_5/offboard/command', AckermannDrive, queue_size = 1)
@@ -33,73 +26,57 @@ def bounds(value,min_val,max_val):
 #function to control steering and velocity
 def control(data):
 	global prev_error
+	global prev_angle
 	global vel_input
 	global kp
 	global kd
-
+	global vel_scale
 
 	rospy.loginfo('PID Control Node is Listening to error')
 
-	## Your PID code goes here
-	#TODO: Use kp, ki & kd to implement a PID controller
 	# 1. Scale the error
-    scaled_error = kp * data.pid_error
+	scaled_error = kp * data.pid_error
 
-	# 2. Apply the PID equation on error to compute steering, missing ki helps vehicle get back on track if it runs over something which causes it to get off course
+	# 2. Apply the PID equation (PD form)
+	v_theta = scaled_error + (kd * (data.pid_error - prev_error))
 
-	v_theta = scaled_error + (kd * ( data.pid_error - prev_error)) #formula for the PID equation
+	# 3. Update steering angle using accumulated effect
+	angle = prev_angle + v_theta
+	prev_angle = angle
 
-	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
+	# 4. Store current error for derivative use
+	prev_error = data.pid_error
+
+	# 5. Clamp steering
 	command = AckermannDrive()
-
-    #define angle
-
-    angle = prev_error + v_theta
-    prev_error = angle
-	# TODO: Make sure the steering value is within bounds [-100,100]
 	steering_angle = -angle + servo_offset
-	clip_steering_angle = bounds(steering_angle,-100,100)
-    rospy.loginfo("Steering Angle = %.2f | Clipped = %.2f" , steering_angle , clip_steering_angle)
-	if clip_steering_angle == steering_angle:
-	   command.steering_angle = clip_steering_angle
-    else:
-        rospy.loginfo('Warning: Error in Angle')
-        command.steering_angle = clip_steering_angle
-	# TODO: Make sure the velocity is within bounds [0,100]
+	clip_steering_angle = bounds(steering_angle, -100, 100)
+	rospy.loginfo("Steering Angle = %.2f | Clipped = %.2f", steering_angle, clip_steering_angle)
+	command.steering_angle = clip_steering_angle
 
-     # Adjust speed dynamically based on wall-following error
-     mag = vel_input - vel_scale * abs(data.pid_error)   # reduce speed when error increases
+	# 6. Adjust speed dynamically based on wall-following error
+	mag = vel_input - vel_scale * abs(data.pid_error)
 
-     if data.pid_error >= 1:
-         speed = 0.7 * data.pid_vel + 0.3 * mag
-     else:
-         speed = mag
+	if data.pid_error >= 1:
+		speed = 0.7 * getattr(data, 'pid_vel', 0) + 0.3 * mag  # safe if pid_vel doesn't exist
+	else:
+		speed = mag
 
-     clip_speed = bounds(speed, 0, 100)
-     if clip_speed == speed:
-         command.speed = clip_speed
-     else:
-         rospy.loginfo('Warning: Error in Speed')
-     command.speed = clip_speed
-    prev_error = data.pid_error
+	clip_speed = bounds(speed, 0, 100)
+	if clip_speed != speed:
+		rospy.loginfo('Warning: Error in Speed')
+	command.speed = clip_speed
 
-	# Move the car autonomously
+	# 7. Publish command
 	command_pub.publish(command)
 
-
 if __name__ == '__main__':
-
-    # This code tempalte asks for the values for the gains from the user upon start, but you are free to set them as ROS parameters as well.
-	global kp
-	global kd
-	global ki
-	global vel_input
-	kp = input("Enter Kp Value: ")
-	kd = input("Enter Kd Value: ")
-	ki = input("Enter Ki Value: ")
-	vel_input = input("Enter desired velocity: ")
+	global kp, kd, ki, vel_input, vel_scale
+	kp = float(input("Enter Kp Value: "))
+	kd = float(input("Enter Kd Value: "))
+	ki = float(input("Enter Ki Value: "))
+	vel_input = float(input("Enter desired velocity: "))
+	vel_scale = float(input("Enter Velocity Scaling Value: "))
 	rospy.init_node('pid_controller', anonymous=True)
-    # subscribe to the error topic
 	rospy.Subscriber("error", pid_input, control)
-
 	rospy.spin()
