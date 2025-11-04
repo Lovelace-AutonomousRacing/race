670962f32ab53a02fe7ef4ba7350dd07847aa6bd
sheri-na <sherina.l.williams@gmail.com>
2025-11-04 13:08:36 -0500

    updated the scan message

--- src/control.py
+++ src/control.py
@@ -20,6 +20,30 @@ prev_error = 0.0
 # 15: Very Slow (Good for debug mode)
 # 25: Slow and steady
 # 35: Nice Autonomous Pace
+#!/usr/bin/env python
+import math
+import rospy
+from race.msg import pid_input
+from ackermann_msgs.msg import AckermannDrive
+
+#PID Control Params
+kp = 6.0 #TODO
+kd = 0.8 #TODO
+ki = 0.0 #TODO
+vel_max = 35.0 #changed in input
+vel_min = 25.0
+servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
+prev_error = 0.0
+
+
+
+
+# This code can input desired velocity from the user.
+# velocity must be between [0,100] to move forward.
+# The following velocity values correspond to different speed profiles.
+# 15: Very Slow (Good for debug mode)
+# 25: Slow and steady
+# 35: Nice Autonomous Pace
 # > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
 
 
@@ -29,7 +53,7 @@ command_pub = rospy.Publisher('/car_5/offboard/command', AckermannDrive, queue_s
 
 #function to clip
 def bounds(value,min_val,max_val):
-    return max(min_val, min(value,max_val))
+	return max(min_val, min(value,max_val))
 
 #function to control steering and velocity
 def control(data):
@@ -44,7 +68,7 @@ def control(data):
 	## Your PID code goes here
 	#TODO: Use kp, ki & kd to implement a PID controller
 	# 1. Scale the error
-    scaled_error = kp * data.pid_error *10
+	scaled_error = kp * data.pid_error *10
 
 	# 2. Apply the PID equation on error to compute steering, missing ki helps vehicle get back on track if it runs over something which causes it to get off course
 
@@ -53,46 +77,29 @@ def control(data):
 	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
 	command = AckermannDrive()
 
-    #define angle
+	#define angle
 
-    angle =  v_theta
+	angle =  v_theta
 
 	# TODO: Make sure the steering value is within bounds [-100,100]
 	steering_angle = -angle + servo_offset
 	clip_steering_angle = bounds(steering_angle,-100,100)
-    rospy.loginfo("Steering Angle = %.2f | Clipped = %.2f" , steering_angle , clip_steering_angle)
+	rospy.loginfo("Steering Angle = %.2f | Clipped = %.2f" , steering_angle , clip_steering_angle)
 	if clip_steering_angle == steering_angle:
-	   command.steering_angle = clip_steering_angle
-    else:
-        rospy.loginfo('Warning: Error in Angle')
-        command.steering_angle = clip_steering_angle
-	# TODO: Make sure the dynamic velocity is within bounds [0,100]
-    vel_max = 45.0                 #max speed
-    vel_min = 25.0               # minimum speed for tight turns
-    k_vel   = 25.0               #how aggresive to slow down
-
-    turn_ratio = abs(clip_steering_angle)/100.0
-    speed = vel_max - k_vel * turn_ratio
-
-    if(data.pid_error >= 1):
-        dynamic_vel = 0.7 * data.pid_vel + 0.3 * speed
-    else:
-        dynamic_vel = speed
-
-    # limit to user range
-    target_vel = bounds(dynamic_vel, vel_min, vel_max)
-    # clip to command range
-    clip_dynamic_vel = bounds(target_vel, 0, 100)
-
-    rospy.loginfo("Dynamic Velocity = %.2f | Clipped = %.2f" , dynamic_vel , clip_dynamic_vel)
-
-	if clip_dynamic_vel == target_vel:
-		command.speed= clip_dynamic_vel
+		command.steering_angle = clip_steering_angle
 	else:
-		rospy.loginfo('Warning: Error in Speed')
-		command.speed = clip_dynamic_vel
+		rospy.loginfo('Warning: Error in Angle')
+		command.steering_angle = clip_steering_angle
+	# TODO: Make sure the dynamic velocity is within bounds [0,100]
+	# vel_max = 45.0                 #max speed
+	vel_min = 25.0               # minimum speed for tight turns
+	k_vel   = 25.0               #how aggresive to slow down
 
-    prev_error = data.pid_error
+	dynamic_vel = ((vel_max-vel_min)/2)*(math.sin((math.pi*clip_steering_angle)/100.0 + math.pi/2.0)+1) + vel_min
+	#sin curve centered around 0 using steering range [-100, 100] and velocity (y) range vel_min to vel_max
+	rospy.loginfo("dynamic velocity = %.2f", dynamic_vel)
+	command.speed = dynamic_vel
+	prev_error = data.pid_error
 
 	# Move the car autonomously
 	command_pub.publish(command)
@@ -100,7 +107,7 @@ def control(data):
 
 if __name__ == '__main__':
 
-    # This code tempalte asks for the values for the gains from the user upon start, but you are free to set them as ROS parameters as well.
+	# This code tempalte asks for the values for the gains from the user upon start, but you are free to set them as ROS parameters as well.
 	global kp
 	global kd
 	global ki
@@ -110,7 +117,7 @@ if __name__ == '__main__':
 	ki = input("Enter Ki Value: ")
 	vel_max = input("Enter max desired velocity: ")
 	rospy.init_node('pid_controller', anonymous=True)
-    # subscribe to the error topic
+	# subscribe to the error topic
 	rospy.Subscriber("error", pid_input, control)
 
 	rospy.spin()
--- src/scan_test.py
+++ src/scan_test.py
@@ -3,14 +3,12 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from dist_finder import find_disparities
+
 def scan_callback(data):
     disparities = find_disparities(data.ranges)
-    rospy.loginfo(f"Found disparities at: {disparities}")
-
-def callback(data):
-    print data.ranges[540]
+    rospy.loginfo("Found disparities at: {}".format(disparities))
 
 rospy.init_node("scan_test", anonymous=False)
-sub = rospy.Subscriber("/car_5/scan", LaserScan, callback)
+sub = rospy.Subscriber("/car_5/scan", LaserScan, scan_callback)
 rospy.spin()
 
