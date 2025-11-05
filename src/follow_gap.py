5ec4015a03223dcd2e849f9e1e6125217c927cea
ericzn248 <ericzn248@gmail.com>
2025-11-04 21:17:17 -0500

    restructure code

--- src/follow_gap.py
+++ src/follow_gap.py
@@ -15,35 +15,34 @@ def findDisparity(data):
     # angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
     angle_increment = data.angle_increment  # angle between each value in ranges
 
-    ranges = data.ranges
+    ranges = [i for i in data.ranges]
     disparities = []
     # step 1 find the disparities in range
     for i in range(1,len(ranges)):
         if abs(ranges[i] - ranges[i-1]) > threshold:
             #append the index of points to represent the disparity
-            disparities.append(i)
+            disparities.append((i-1, i))
     # step 2 mask out the disparity that is unsafe for car to pass
     # TODO: find a good tolerance
     tolerance = 0.0
 
-    # (index of right most in current disparity, distance to this disparity)
-    filtered_disparities = None
     for i in disparities:
-        close_dist = min(ranges[disparities[i]], ranges[disparities[i-1]])
+        if ranges[disparities[i][0]] > ranges[disparities[i][1]]:
+            extend_right = False  # tells us whether to extend right or left
+            close_dist = ranges[disparities[i][0]]
+        else:
+            extend_right = True
+            close_dist = ranges[disparities[i][1]]
+
+        close_dist = min(ranges[disparities[i][0]], ranges[disparities[i]])
         numbers_scan= math.ceil(math.radians((tolerance+car_width/2)/(2*math.pi*close_dist))/angle_increment)
-        
-        for index in range(1,numbers_scan):
-            # mark BAD disparity[index]= -1 means the gap is unsafe to pass
-            if ranges[index+i] < close_dist or i + index >= len(ranges):
-                disparities[i] = -1
-                break
-            if index == numbers_scan-1:
-                filtered_disparities.append((i+index,close_dist))
+
+        # TODO: extend disparities by changing ranges
 
     # step 3 find the farthest reachable distance
     dis = -1
     index = -1  #refer to the index of point in ranges
-    for i, distance in filtered_disparities:
+    for i, distance in enumerate(ranges):
         if distance>dis:
             dis = distance
             index = i
